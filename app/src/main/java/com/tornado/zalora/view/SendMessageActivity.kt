5a3f431c8411a86494f4714e0492322f0150c89d
Duy khanh <kasuko.go@gmail.com>
2018-09-20 00:04:06 +0700

    create new repository

---
+++ app/src/main/java/com/tornado/zalora/view/SendMessageActivity.kt
@@ -0,0 +1,13 @@
+package com.tornado.zalora.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tornado.zalora.R
+
+class SendMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_message)
+    }
+}
---
+++ app/src/main/java/com/tornado/zalora/view/adapter/MessageAdapter.kt
@@ -0,0 +1,2 @@
+package com.tornado.zalora.view.adapter
+
